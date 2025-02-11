f5e2dabd6d80f1c210bd77311236931e8f04873e
Phil Massyn <phil.massyn@icloud.com>
2025-02-11 10:37:27 +1000

    Add qemu check

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+check_qemu() {
+  if [ -f /sys/class/dmi/id/product_name ]; then
+    if grep -qi "qemu" /sys/class/dmi/id/product_name; then
+      echo "Instance is running under QEMU (detected in DMI data)"
+      return 0
+    fi
+  fi
+}
+
 wait_until_time() {
     target_time="$1"
     echo "$(date '+%H%M') - waiting until $target_time....."
@@ -13,7 +22,11 @@ cd cyber-dashboard-flask
 service nginx start
 
 cd server
-gunicorn -w 4 -b 0.0.0.0:8080 app:server &
+if check_qemu; then
+    python app.py &
+else
+    gunicorn -w 4 -b 0.0.0.0:8080 app:server &
+fi
 cd ../..
 
 # == start the data collection
