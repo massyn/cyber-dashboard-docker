8504447290a41cc7a6684b7ee32071463684e141
Phil Massyn <phil.massyn@icloud.com>
2025-02-19 06:30:23 +1000

    Test install script

--- install.sh
+++ install.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
+detect_os() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        case "$ID" in
+            "amzn")
+                echo "amzn"
+                return
+                ;;
+            "ubuntu")
+                echo "ubuntu"
+                return
+                ;;
+            *)
+                echo "none"
+                return
+                ;;
+        esac
+    else
+        echo "none"
+        return
+    fi
+}
+os_type=$(detect_os)
+echo "Detected OS: $os_type"
+
 # Install all basic packages
-if command -v yum >/dev/null 2>&1; then
+if [ "$os_type" = "amzn" ]; then
     yum update -y
     yum install git -y
     yum install nginx -y
     yum install unzip -y
     yum remove python3-requests -y
-else
+elif [ "$os_type" = "ubuntu" ]; then
     apt-get update
     apt-get upgrade -y
 
@@ -15,7 +40,12 @@ else
     apt-get install python3 python3-pip -y
     apt-get install python3.12-venv -y
     apt-get install nginx -y
+else
+    echo "OS not supported for automated updates."
+    exit 1
 fi
+
+STARTPATH=$(pwd)
 
 # You may want to install the AWS CLI (or any other Cloud provider's CLI at this point)
 curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip" \
@@ -23,56 +53,46 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv
     && ./aws/install \
     && rm awscliv2.zip
 
-# == configure nginx
-if [ -f /etc/nginx/sites-enabled/default ]; then
-    cp nginx.conf /etc/nginx/sites-enabled/default
-else
-    cp nginx.conf /etc/nginx/conf.d/app.conf
-fi
-
-if command -v python3 >/dev/null 2>&1; then
-    export PYTHON=python3
-else
-    export PYTHON=python
-fi
-
-# == copy flask app
-#cp flaskapp.service /etc/systemd/system
-
-# == create the local user
-useradd -r -g 0 cyberdashboard
-
-# == we create a separate folder to keep all of it
 mkdir /usr/bin/dashboard
 cd /usr/bin/dashboard
 
-# == We create a new python virtual environment to keep things clean
-$PYTHON -m venv .venv
-. .venv/bin/activate
-
-# == let's go grab our main application
 git clone https://github.com/massyn/cyber-dashboard-flask
 git clone https://github.com/massyn/cyber-metrics
+
+# == We create a new python virtual environment to keep things clean
+if [ "$os_type" = "amzn" ]; then
+    python -m venv .venv
+elif [ "$os_type" = "ubuntu" ]; then
+    python3 -m venv .venv
+fi
+
+. .venv/bin/activate
 python -m pip install --no-cache-dir -r cyber-dashboard-flask/requirements.txt
 python -m pip install --no-cache-dir -r cyber-metrics/requirements.txt
 
-# # == TODO configure the flask app
-# if command -v systemctl >/dev/null 2>&1; then
-#     systemctl daemon-reload
-#     systemctl start flaskapp
-# else
-#     service flaskapp start
-# fi
+# == Configure Flask service
 
-# Start nginx
+if [ "$os_type" = "amzn" ]; then
+    cp ${STARTPATH}/flaskapp.service /etc/systemd/system
+    systemctl daemon-reload
+    systemctl start flaskapp
+    systemctrl enable flaskapp
+elif [ "$os_type" = "ubuntu" ]; then
+    echo TODO
+fi
 
-if command -v systemctl >/dev/null 2>&1; then
-    echo "Using systemctl to manage nginx..."
+# == configure nginx
+if [ -f /etc/nginx/sites-enabled/default ]; then
+    cp ${STARTPATH}/nginx.conf /etc/nginx/sites-enabled/default
+else
+    cp ${STARTPATH}/nginx.conf /etc/nginx/conf.d/app.conf
+fi
+
+if [ "$os_type" = "amzn" ]; then
     systemctl stop nginx
     systemctl start nginx
     systemctl enable nginx
-else
-    echo "Using service to manage nginx..."
+elif [ "$os_type" = "ubuntu" ]; then
     service nginx stop
     service nginx start
     if [ ! -f /etc/rc.local ]; then
@@ -81,3 +101,4 @@ else
     fi
     grep -q "service nginx start" /etc/rc.local || echo "service nginx start" >> /etc/rc.local
 fi
+
